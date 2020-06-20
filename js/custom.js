250059705e56984f5bd7c49b4bc1f56b0e706843
Jonias Fortuna <jonias@potato.id>
2020-06-21 03:15:30 +0700

    change

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,7 @@
 window.onload=function(){document.getElementById("loading").style.display="none"}
+function showComments() {
+	var disqusButton = document.getElementById('disqus-button');
+	disqusButton.parentNode.removeChild(disqusButton); 
+	var disqusComments = document.getElementById('disqus-comments');
+	disqusComments.style.display = 'block'; 
+}
