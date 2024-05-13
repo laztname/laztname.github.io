098f01cb9be1e3d37bcd74e75db9e7a5fe6dc104
kali <kali@potato.id>
2024-05-13 05:54:44 -0700

    remove show comment js

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,1 @@
 window.onload=function(){document.getElementById("loading").style.display="none"}
-function showComments() {
-	var disqusButton = document.getElementById('disqus-button');
-	disqusButton.parentNode.removeChild(disqusButton); 
-	var disqusComments = document.getElementById('disqus-comments');
-	disqusComments.style.display = 'block'; 
-}
