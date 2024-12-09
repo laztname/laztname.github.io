2627b76268f7f1d7262ece85e70537ab40ba55b5
laztname <39350055+laztname@users.noreply.github.com>
2024-12-09 21:30:55 +0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,1 @@
-window.onload=function(){document.getElementById("loading").style.display="none"}
+window.onload = () => document.getElementById("loading").style.display = "none"; setTimeout(() => document.getElementById("loading").style.display = "none", 5000);
