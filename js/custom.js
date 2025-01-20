77c4eff4b310ab1d4c258ca36060e7a45ca18812
kali <kali@potato.id>
2025-01-20 16:16:29 +0700

    layouts: shorterining title add js to add suffix

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,2 @@
+document.title += " # Potato Blog";
 window.onload = () => document.getElementById("loading").style.display = "none"; setTimeout(() => document.getElementById("loading").style.display = "none", 5000);
