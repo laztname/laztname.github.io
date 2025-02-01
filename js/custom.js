46c04848ca18d26cffdafc7b4c276ba3b380b7b1
kali <kali@potato.id>
2025-02-02 02:45:59 +0700

    posts:

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,1 @@
-document.title += " # Potato Blog";
 window.onload = () => document.getElementById("loading").style.display = "none"; setTimeout(() => document.getElementById("loading").style.display = "none", 5000);
