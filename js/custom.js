a705689679558bf723925431bb22e2484b33fa37
laztname <laztname@users.noreply.github.com>
2025-07-05 19:38:48 +0000

    deploy: laztname/website-backend@071bd09bc424b8890e4d745de5f8f93f77cac00a

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,1 @@
-window.onload = () => document.getElementById("loading").style.display = "none"; setTimeout(() => document.getElementById("loading").style.display = "none", 5000);
+window.onload = () => document.getElementById("loading").style.display = "none"; setTimeout(() => document.getElementById("loading").style.display = "none", 3000);
